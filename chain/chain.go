42f403e563a9f6dbb615a44ee23a017185be2ec7
Sivan Mehta <Sivanmehta2@gmail.com>
2019-06-07 23:55:50 -0700

    break to a different file

--- chain/chain.go
+++ chain/chain.go
@@ -2,19 +2,13 @@ package chain
 
 import (
 	"math/rand"
-	"io/ioutil"
 )
 
-var corpus []byte
-
 //
-// GenerateChain will return a random piece of the Declaration of the Independence
+// GenerateChain will return a random piece of the gathered tweets
 //
 func GenerateChain() []byte {
-	if corpus == nil {
-		corpus, _ = ioutil.ReadFile("corpus.txt")
-	}
-	
+	corpus := getTweets()
   // we will randomly slice this later
   start := rand.Intn(len(corpus))
   end := start + rand.Intn(len(corpus) - start)
