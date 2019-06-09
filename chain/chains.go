1b491dedcd6d3863cab5cff6f420d02f9f2fccd2
Sivan Mehta <Sivanmehta2@gmail.com>
2019-06-08 17:52:20 -0700

    || and a litany of comments

--- chain/chains.go
+++ chain/chains.go
@@ -2,25 +2,28 @@ package chain
 
 import (
   "fmt"
-  // "time"
-  // "reflect"
+  "sync"
 )
 
+type bandwagon func() string
 // Key: Trending Topic on Twitter
 // Value: Function that generates a tweet from the keyed topic
-type bandwagons map[string]func() string
+type bandwagons map[string]bandwagon
 var currentBandwagons bandwagons
 var upcomingBandwagons bandwagons
 
-func makeChain(trend string) {
+func createGenerator(trend string, pointer *bandwagon, pool *sync.WaitGroup) {
   fmt.Println("Generating Markov chain for", trend)
 
-  // BUILD GATHER TWEETS FROM GIVEN TREND
-  // BUILD A NGRAM DICTIONARY FROM THE TWEETS
-  // ASSIGN A FUNCTION THAT USES THAT DICTIONARY
-  upcomingBandwagons[trend] = func() string {
-    return "Raptors 4 life"
+  // Gather tweets from a given trend
+  // Build an n-gram dictionary from the tweets
+  // Generate a function that uses that dictionary to return a tweet
+  // (THIS IS THE HARD PART)
+  *pointer = func() string {
+    return "Lifelong Warriors fan since 2015"
   }
+
+  pool.Done() // decrement the counter
 }
 
 //
@@ -31,12 +34,21 @@ func MakeChains() {
   upcomingBandwagons = make(bandwagons)
   trends := getTrends()
 
-  // figure how to make concurrent, because that will become blocking if done poorly
-  for _, trend := range trends {
-    makeChain(trend)
+  // a pool so that we can generate the bandwagons in parallel
+  var pool sync.WaitGroup
+  // an array of pointers that we'll use to track the generated trends
+  generators := make([]bandwagon, len(trends))
+  for i, trend := range trends {
+    pool.Add(1) // increment the counter
+    go createGenerator(trend, &generators[i], &pool)
+  }
+  pool.Wait() // wait until the counter is at 0
+  // now we have two analogous arrays, trends and generators that represent the upcoming bandwagon
+  // so we can very quickly arrange build the upcomingBandwagon map
+  for i, trend := range trends {
+    upcomingBandwagons[trend] = generators[i]
   }
 
-  
   // reset what bandwagon we're currently on
   currentBandwagons = upcomingBandwagons
 }
@@ -48,7 +60,6 @@ func join(strs ...string) string {
 		ret += str
 	}
   return ret
-  
 }
 
 //
